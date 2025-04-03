059a686e3ee55d1d869fc7b7ca0d78ca94e45c6b
Ubuntu <ubuntu@ip-172-31-93-21.ec2.internal>
2025-04-03 05:17:25 +0000

    add llava next support (ongoing)

---
+++ examples/multimodal/convert_llava_next_pretrain_to_wds.py
@@ -0,0 +1,36 @@
+import json
+import os
+import webdataset as wds
+
+from tqdm import tqdm
+
+# llava_pretrain_dir = '/workspace/dataset/LLaVA-NeXT-Data/llava_next_raw_format'
+llava_pretrain_dir = '/home/ubuntu/dataset/LLaVA-NeXT-Data/llava_next_raw_format'
+
+# Paths to the dataset files
+json_file = os.path.join(llava_pretrain_dir, 'llava_next_raw_format_processed.json')
+output = os.path.join(llava_pretrain_dir, 'wds')
+
+if not os.path.exists(output):
+    os.mkdir(output)
+
+# Load data
+with open(json_file, 'r') as f:
+    data = json.load(f)
+
+with wds.ShardWriter(os.path.join(output, 'pretrain-%d.tar'), maxcount=10000) as shard_writer:
+    for entry in tqdm(data):
+        # if 'llava_pretrain_lcs558k' in entry['image']:
+        try:
+            with open(os.path.join(llava_pretrain_dir, entry['image']), "rb") as img_file:
+                    image_data = img_file.read()
+            sample = {
+                "__key__": entry['id'],
+                "jpg": image_data,
+                "json": json.dumps(entry['conversations']).encode("utf-8"),
+            }
+            shard_writer.write(sample)
+        except Exception as e:
+            print('Error:', entry['image'], e)
+
+print(f"Dataset successfully converted to wds")
